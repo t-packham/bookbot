d290cf1140cc8cc05a8092faa7684354d67e5678
t-packham <packhamte@gmail.com>
2024-11-10 14:21:44 -0800

    word count

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 def main(): 
     book_path = "books/frankenstein.txt"
     text = open_file(book_path)
-    print(text)
+    count = count_words(text)
+    print(count)
+
 def open_file(path):
     with open(path) as f:
         return f.read()
+
+def count_words(books):
+    words = books.split()
+    count = len(words)
+    return count
 
 
 main()
